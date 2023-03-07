afc3e887b7c6e46ebfe4304b2546af9f69b9d9c9
sheama <shesler200@mail.ru>
2023-03-07 12:54:25 +0700

    home

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
+        sale: '',
         selectedVariant: 0,
         altText: "A pair of socks",
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
@@ -11,13 +12,15 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
+                variantQuantity: 10,
+                variantSale: true
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
+                variantQuantity: 0,
+                variantSale: true
             }
         ],
         cart: 0
@@ -35,14 +38,19 @@ let app = new Vue({
     },
     computed: {
         title() {
-            return this.brand + ' ' + this.product;
+            return this.brand + ' ' + this.product + ' ' + this.sale;
         },
         image() {
             return this.variants[this.selectedVariant].variantImage;
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity;
+        },
+        onSale(){
+            return this.variants[this.selectedVariant].variantSale;
         }
+
+
     },
 
 
